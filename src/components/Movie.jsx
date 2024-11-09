3b44472c22d9b6187145aecf020f191124262dd9
restucode <restu.kersana1@gmail.com>
2024-11-09 11:16:44 +0700

    fix Movie card

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -5,9 +5,11 @@ const Movie = ({ allMovie }) => {
             <div className="row">
                 {allMovie.allMovie && allMovie.allMovie.map((movie, index) => (
                     <div style={{ width: '20%', padding: '20px', }} key={index}>
-                        <div className="card">
-                            <img src={movie.Poster} alt="" className="card-img-top" />
-                            <div className="card-body text-center" style={{ backgroundColor : '#EC8305', color: 'white' }}>
+                        <div className="card" style={{ height : '450px'}}>
+                            <img src={movie.Poster} alt="" style={{ height: '350px', objectFit: 'cover' }} className="card-img-top" onError={(event) => {
+                                event.target.src = "https://cdn.pixabay.com/photo/2021/01/10/20/03/laptop-5906264_1280.png"
+                            }} />
+                            <div className="card-body text-center d-flex justify-content-center align-items-center" style={{ backgroundColor : '#EC8305', color: 'white' }}>
                                 <div className="card-text">{movie.Title}</div>
                             </div>
                         </div>
